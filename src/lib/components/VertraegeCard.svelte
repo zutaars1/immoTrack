23fed412d3efe75ab8825a4769e45e24eb41c709
zutaars1 <zutaars1@students.zhaw.ch>
2025-05-31 00:06:02 +0200

    new Files

--- src/lib/components/VertraegeCard.svelte
+++ src/lib/components/VertraegeCard.svelte
@@ -2,14 +2,17 @@
   let { vertrag } = $props();
 </script>
 
-<div class="card shadow-sm h-100">
-  <div class="card-body">
-    <h5 class="card-title">{vertrag.name_mieter}</h5>
-    <p class="card-text">
-      <strong>Startdatum:</strong> {vertrag.startdatum}<br />
-      <strong>E-Mail:</strong> {vertrag.email}<br />
-      <strong>Telefon:</strong> {vertrag.telefonnummer}<br />
-      <strong>Mietzins:</strong> {vertrag.mietzins}<br />
-    </p>
+<div class="card shadow-sm h-100 d-flex flex-column">
+  <div class="card-body d-flex flex-column justify-content-between">
+    <div>
+      <h5 class="card-title">{vertrag.name_mieter}</h5>
+      <p class="card-text">
+        <strong>Startdatum:</strong> {vertrag.startdatum}<br />
+      </p>
+    </div>
+
+    <div class="mt-3">
+      <a class="btn btn-outline-primary w-100" href={`/vertraege/${vertrag._id}`}>Details</a>
+    </div>
   </div>
 </div>
